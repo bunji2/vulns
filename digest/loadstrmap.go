37122270a0737fc6734c3418c8cf8305f069d428
Bunji Square <bunji2@users.noreply.github.com>
2020-02-24 02:23:47 +0900

    Upload

--- digest/loadstrmap.go
+++ digest/loadstrmap.go
@@ -5,25 +5,6 @@ import (
 	"io/ioutil"
 )
 
-// loadStrMap は JSON 形式で保存された文字列マップを読み出す関数
-func loadStrMap(filePath string) (r map[string]string, err error) {
-	r = map[string]string{}
-
-	// バイト列読み出し
-	var bytes []byte
-	bytes, err = ioutil.ReadFile(filePath)
-	if err != nil {
-		return
-	}
-
-	// json 形式のデコード
-	err = json.Unmarshal(bytes, &r)
-	if err != nil {
-		return
-	}
-	return
-}
-
 // loadStrArrayMap は JSON 形式で保存された文字列マップを読み出す関数
 func loadStrArrayMap(filePath string) (r map[string][]string, err error) {
 	r = map[string][]string{}
