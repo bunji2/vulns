655e6dedef5297f08c760ad5489e0e6e5904a0fe
Bunji Square <bunji2@users.noreply.github.com>
2020-03-11 19:41:43 +0900

    Add comments

--- jvn/jvn.go
+++ jvn/jvn.go
@@ -3,6 +3,13 @@
 // [MEMO] jvndb から RDF をダウンロードし、脆弱性レポートデータの作成にあたり、
 // Golang 標準の XML パーサを使うと大量のメモリを消費するため、メモリ影響をかけない形でパースする。
 
+// [XXX] 以下の問題があるが、すぐには解決できそうにない。
+// ・大量のメモリを消費せずに XML をパースするいいライブラリが見つかっていないこと。
+// ・従って自前の実装コードで十分にテストができていると断言できず、バグが潜んでいる可能性があること。
+// ・JPCERT/CC の実装に依存していること。（これは大丈夫なのではないか、と楽観的にみている）
+//   - JVNDB のサイトが出力しているタグの改行の作法に依存しているため、もしも仕様が変更されると全面的な作り直しが発生する。
+//   - JVNDB のサイトの URL 仕様に依存しているため、仕様が変更されると urlFmt の修正が必要となる。
+
 package jvn
 
 import (
@@ -17,9 +24,11 @@ import (
 )
 
 const (
+	// 接続先 URL のフォーマット
 	urlFmt = "https://jvndb.jvn.jp/ja/feed/detail/jvndb_detail_%04d.rdf"
 )
 
+// CVEID の正規表現を格納する変数
 var regCvePat *regexp.Regexp
 
 // Init はパッケージを初期化する関数
@@ -30,13 +39,15 @@ func Init() (err error) {
 
 // Fetch は指定された西暦年の脆弱性レポートを取得する関数
 func Fetch(year int) (r []vulns.VulnReport, err error) {
-	// year のチェック
+	// year のチェック。ここでしくじると、JVNDB のサイトに無駄なアクセスが発生するので注意。
+	// 指定されるべき正しい値は、1998 <= year <=現在の西暦年
 	t := time.Now()
 	if year > t.Year() || year < 1998 {
 		err = fmt.Errorf("abnormal year")
 		return
 	}
 
+	// 指定された西暦年をもとに URL を作成し接続する。
 	url := fmt.Sprintf(urlFmt, year)
 	var resp *http.Response
 	resp, err = http.Get(url)
@@ -46,6 +57,7 @@ func Fetch(year int) (r []vulns.VulnReport, err error) {
 	defer resp.Body.Close()
 
 	// [TODO] コンテンツタイプの確認。
+	// が、いまのところコンテンツタイプの確認をしなくても致命的な問題は起きていない。。。
 
 	// レスポンスを一行ずつ処理
 	sc := bufio.NewScanner(resp.Body)
@@ -54,9 +66,16 @@ func Fetch(year int) (r []vulns.VulnReport, err error) {
 			break
 		}
 		var record vulns.VulnReport
+		
+		// 前後の空白のトリミング
 		line := strings.TrimSpace(sc.Text())
+		
+		// Vulinfo のタグが見つかったら readVulinfo を呼ぶ。
+		// ここが独自パージングの起点。
 		if strings.Contains(line, "<Vulinfo>") {
 			record, err = readVulinfo(sc)
+			// readVulinfo の先の処理の中で、</Vulinfo> まで読み込んでいる（はず）。
+			
 			r = append(r, record)
 		}
 		//fmt.Println(record)
@@ -65,16 +84,21 @@ func Fetch(year int) (r []vulns.VulnReport, err error) {
 	return
 }
 
+// readVulinfo は Vulinfo ノードのパースを行う関数
 func readVulinfo(sc *bufio.Scanner) (r vulns.VulnReport, err error) {
 	var id, title, overview, impact string
 	var cpes, cves, cvsss []string
-	for sc.Scan() {
+	for sc.Scan() { // [LOOP1]
 		if sc.Err() != nil {
 			break
 		}
+		// 一行読み出し、前後の空白をトリミング
 		line := strings.TrimSpace(sc.Text())
+		
+		// </Vulinfo> を見つけたら脆弱性レポートデータを作ってループを抜ける。
 		if strings.Contains(line, "</Vulinfo>") {
 			if id != "" && title != "" && overview != "" && impact != "" {
+				// cpes, cves, cvsss は空でもよい
 				r = vulns.VulnReport{
 					ID:       id,
 					Title:    title,
@@ -87,51 +111,65 @@ func readVulinfo(sc *bufio.Scanner) (r vulns.VulnReport, err error) {
 			}
 			break
 		} else if strings.Contains(line, "<Related>") {
+			// <Related> を見つけたら cveid を探しにいく
 			cves, err = readCVEs(sc)
 			if err != nil {
 				break
 			}
 		} else if id == "" && strings.Contains(line, "<VulinfoID>") {
+			// <VulinfoID> を見つけたら脆弱性レポートの ID を取り出す
 			id = readVulinfoID(line)
 		} else if title == "" && strings.Contains(line, "<Title>") {
+			// <Title> を見つけたら脆弱性レポートの title を取り出す
 			title = readTitle(line)
 		} else if overview == "" && strings.Contains(line, "<Overview>") {
+			// <Overview> を見つけたら脆弱性レポートの overview を取り出す
 			overview = readOverview(line)
 		} else if impact == "" && strings.Contains(line, "<Description>") {
+			// <Description> を見つけたら脆弱性レポートの impact を取り出す
 			impact = readDescription(line)
 		} else if strings.Contains(line, "<Vector>") {
+			// <Vector> を見つけたら脆弱性レポートの CVSS を取り出す
 			cvsss = append(cvsss, readVector(line))
 		} else if strings.Contains(line, "<Cpe ") {
+			// <Cpe を見つけたら脆弱性レポートの CPE を取り出す
 			cpes = append(cpes, readCpe(line))
 		}
-	}
+		
+	} // [LOOP1]
+	
 	return
 }
 
+// readCVEs は CVEID のリストを読み出す関数
 func readCVEs(sc *bufio.Scanner) (cves []string, err error) {
 	ids := map[string]int{}
 	for sc.Scan() {
 		if sc.Err() != nil {
 			break
 		}
+		// 一行読み出し、前後の空白をトリミング
 		line := strings.TrimSpace(sc.Text())
+		
 		if strings.Contains(line, "</Related>") {
+			// "</Related>" が現れたらループを抜ける
 			break
 		} else if strings.Contains(line, "<VulinfoID>") {
+			// "<VulinfoID>" が現れたら CVEID を取り出す。ここ、脆弱性レポートIDとタグが共用だ。
 			id := readVulinfoID(line)
 			//fmt.Println("id =", id)
+			
+			// 取り出した ID の中には複数の CVEID が含まれている可能性があるので、
+			// CVEID の正規表現のパターンにマッチさせて取り出す。
 			for _, match := range regCvePat.FindAllStringSubmatch(id, -1) {
-				//for _, match := range regCvePat.FindAllStringSubmatch(line, -1) {
 				//fmt.Println("match =", match)
+
+				// map[string]int の ids に格納しているのは、一意な CVEID にしたいから
 				ids[match[0]] = 1
 			}
-			/*
-				if strings.HasPrefix(id, "CVE-") {
-					ids[id] = 1
-				}
-			*/
 		}
 	}
+	// map[string]int の ids から、一意な CVEID リストを取り出す
 	for id := range ids {
 		cves = append(cves, id)
 	}
@@ -168,19 +206,26 @@ func readCpe(line string) (r string) {
 	return
 }
 
+// readTextValue は特定のタグで囲まれた文字列を取り出す関数。
 func readTextValue(tag, line string) (r string) {
 	idx := strings.Index(line, tag)
 	if idx < 0 {
 		return
 	}
+	
+	// "<XXX>SSS</" のようなパターンのときに、SSS の部分だけを取り出す処理
 	line = line[idx+len(tag):]
 	idx = strings.LastIndex(line, "</")
 	if idx < 0 {
 		return
 	}
 	r = line[0:idx]
+	
+	// あとで CSV 化することも想定して二重引用符とカンマは空白に変換
 	r = strings.ReplaceAll(r, `"`, ` `)
 	r = strings.ReplaceAll(r, `,`, ` `)
+	
+	// 前後の空白のトリミングはここではやらない
 	return
 }
 
